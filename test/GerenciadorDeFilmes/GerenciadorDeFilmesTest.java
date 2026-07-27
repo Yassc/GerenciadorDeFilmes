a12da2e9fdb01379c3c56be1360e62a6c92a291d
Yassc <yasmimcastilho2543@gmail.com>
2026-07-26 21:30:54 -0300

    Adição da funcionalidade de avaliação de filmes, exeção de nota inválida e criação do teste para esta função. além disso, ajeitei o nome do pacote, porque não estava condizente.

---
+++ test/GerenciadorDeFilmes/GerenciadorDeFilmesTest.java
@@ -0,0 +1,124 @@
+package GerenciadorDeFilmes;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GerenciadorDeFilmesTest {
+
+    @Test
+    public void testAdicionarEBuscarFilmeComSucesso() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f1 = new Filme("Avatar", "James Cameron", 2009, 162);
+
+        gerenciador.adicionarFilme(f1);
+
+        Filme encontrado = gerenciador.buscarPorTitulo("AVATAR");
+
+        assertNotNull(encontrado);
+        Assert.assertEquals("James Cameron", encontrado.getDiretor());
+    }
+
+    @Test
+    public void testSobrescritaOuAtualizacao() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f2 = new Filme("matrix", "Lana e Lilly Wachowski", 1999, 136);
+        gerenciador.adicionarFilme(f2);
+
+<<<<<<< HEAD:src/GerenciadorDeFilmes/GerenciadorDeFilmesTest.java
+        Filme f3 = new Filme("matrix", "Lana e Lilly Wachowski", 2021, 136);
+=======
+        Filme f3 = new Filme("matrix", "Lana e Lilly Wachowski", 2021, 136 );
+>>>>>>> 94702aa (adicao pom):test/GerenciadorDeFilmes/GerenciadorDeFilmesTest.java
+        gerenciador.adicionarFilme(f3);
+
+        assertEquals(2021, gerenciador.buscarPorTitulo("matrix").getAno());
+    }
+
+    @Test
+    public void testIgnorarLetraMinuscula() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f4 = new Filme("La la land", "Damien Chazelle", 2016, 128);
+        gerenciador.adicionarFilme(f4);
+
+<<<<<<< HEAD:src/GerenciadorDeFilmes/GerenciadorDeFilmesTest.java
+        Filme encontrado = gerenciador.buscarPorTitulo("LA LA LAND");
+        assertNotNull(encontrado);
+        assertEquals("Damien Chazelle", encontrado.getDiretor());
+=======
+        Filme Encontrado = gerenciador.buscarPorTitulo("LA LA LAND");
+        assertNotNull(Encontrado);
+        Assert.assertEquals("Damien Chazelle", Encontrado.getDiretor());
+
+>>>>>>> 94702aa (adicao pom):test/GerenciadorDeFilmes/GerenciadorDeFilmesTest.java
+    }
+
+    @Test(expected = filmeNaoEncontradoException.class)
+    public void testBuscarFilmeInexistenteLancaExcecao() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        gerenciador.buscarPorTitulo("Batman");
+    }
+
+    @Test
+    public void testRemoverPorAnoComSucesso() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f5 = new Filme("Percy Jackson e o Ladrão de Raios", "Chris Columbus", 2010, 118);
+        gerenciador.adicionarFilme(f5);
+
+        String removido = gerenciador.removerPorAno(2010);
+        assertNotNull(removido);
+        assertEquals("Filme do ano 2010 removido com sucesso!", removido);
+    }
+
+    @Test
+    public void testAdicionarAosFavoritosComSucesso() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f6 = new Filme("Coraline", "Henry Selick", 2009, 100);
+        gerenciador.adicionarFilme(f6);
+
+        String mensagem = gerenciador.adicionarAosFavoritos("Coraline");
+
+        assertNotNull(mensagem);
+        assertEquals("O filme Coraline foi adicionado aos seus favoritos!", mensagem);
+    }
+
+    @Test(expected = filmeNaoEncontradoException.class)
+    public void testAdicionarFilmeFantasmaAosFavoritosLancaExcecao() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        gerenciador.adicionarAosFavoritos("Filme Fantasma");
+    }
+
+    @Test
+    public void testAvaliarFilmeComSucesso() throws filmeNaoEncontradoException, NotaDeAvaliacaoInvalidaException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f = new Filme("Inception", "Christopher Nolan", 2010, 148);
+        gerenciador.adicionarFilme(f);
+        assertThrows(filmeNaoEncontradoException.class,
+                () -> {
+                    gerenciador.adicionarAosFavoritos("Filme Fantasma");
+                });
+        gerenciador.avaliarFilme("Inception", 9.5);
+
+        assertEquals(9.5, gerenciador.buscarPorTitulo("Inception").getAvaliacao(), 0.001);
+    }
+
+    @Test(expected = NotaDeAvaliacaoInvalidaException.class)
+    public void testAvaliarFilmeComNotaInvalidaLancaExcecao() throws filmeNaoEncontradoException, NotaDeAvaliacaoInvalidaException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f = new Filme("Interstellar", "Christopher Nolan", 2014, 169);
+        gerenciador.adicionarFilme(f);
+
+        gerenciador.avaliarFilme("Interstellar", 11.0);
+    }
+    @Test
+    public void testMarcarComoAssistidoComSucesso() throws filmeNaoEncontradoException {
+        GerenciadorDeFilmes gerenciador = new GerenciadorDeFilmes();
+        Filme f = new Filme("Matrix", "Wachowski", 1999, 136);
+        gerenciador.adicionarFilme(f);
+
+        String mensagem = gerenciador.marcarComoAssistido("Matrix");
+
+        assertNotNull(mensagem);
+        assertEquals("O filme Matrix foi marcado como assistido!", mensagem);
+    }
+}
